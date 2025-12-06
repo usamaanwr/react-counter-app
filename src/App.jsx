43416ad9dd6f7961eb57f3f376221d5de7f0a094
usamaanwr <anweru98@gmail.com>
2025-12-06 12:44:11 +0500

    Add boundary checks for increase and decrease functions in counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import "./component/styleCss/App.css"
 function App() {
   const [count, setCount] = useState(0)
 const increase = () =>{
-  setCount(count + 1);
+  if (count < 20) {
+    setCount(count +1);
+    
+  }
 }
 const decrease = () =>{
-  setCount(count - 1);
+  if (count > 0) {
+    setCount(count -1);
+  }
 }
 const reset = () =>{
   setCount(0);
